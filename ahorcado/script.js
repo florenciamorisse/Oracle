b56769ff64c310716bd31d831d8bf6b327d9cb28
florenciamorisse <florenciamorisse@gmail.com>
2022-09-09 16:45:49 -0300

    cuando tu botón inicio funciona :)

--- ahorcado/script.js
+++ ahorcado/script.js
@@ -1,7 +1,28 @@
+var btnInicio= document.querySelector(".btn-inicio");
+var titulo= document.querySelector(".title");
+var contenedorTitulo= document.querySelector(".container-titulo");
+var footer= document.querySelector(".footer");
+var juego= document.querySelector(".juego"); 
+var inicio= document.getElementById("inicio");
+
+btnInicio.addEventListener("click", iniciarJuego);
+
+function iniciarJuego() {
+    titulo.classList.add("pag2");
+    contenedorTitulo.classList.add("pag2");
+    footer.classList.add("pag2");
+    juego.classList.remove("desaparecer");
+    inicio.classList.add("desaparecer");
+
+}
+
+
+
 var pantalla = document.querySelector('canvas');
 var pincel= pantalla.getContext('2d');
-pincel.fillStyle= "lightgrey";
-pincel.fillRect(0,0,600,400);
+// color transparent
+pincel.fillStyle= "transparent";
+pincel.fillRect(0,0,1000,700);
 
 
 // var btnIniciar = document.querySelector(".btn-iniciar");
