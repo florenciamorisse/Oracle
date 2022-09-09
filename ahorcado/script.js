af5a5fd2c3b3c77416016292728d6564779cbbbf
florenciamorisse <florenciamorisse@gmail.com>
2022-09-09 16:51:46 -0300

    alinié botones

--- ahorcado/script.js
+++ ahorcado/script.js
@@ -10,7 +10,7 @@ btnInicio.addEventListener("click", iniciarJuego);
 function iniciarJuego() {
     titulo.classList.add("pag2");
     contenedorTitulo.classList.add("pag2");
-    footer.classList.add("pag2");
+    footer.classList.add("pag2footer");
     juego.classList.remove("desaparecer");
     inicio.classList.add("desaparecer");
 
