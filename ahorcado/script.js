61e9d3a9796d94c5a12279ad84cc604bd07cf6f3
florenciamorisse <florenciamorisse@gmail.com>
2022-09-09 12:41:44 -0300

    footer y canvas gris

--- ahorcado/script.js
+++ ahorcado/script.js
@@ -1,3 +1,9 @@
+var pantalla = document.querySelector('canvas');
+var pincel= pantalla.getContext('2d');
+pincel.fillStyle= "lightgrey";
+pincel.fillRect(0,0,600,400);
+
+
 // var btnIniciar = document.querySelector(".btn-iniciar");
 // btnIniciar.addEventListener("click", ocultarBtn);
 
