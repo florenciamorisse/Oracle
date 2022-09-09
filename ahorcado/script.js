8d2ebde349c443cf6c77bc4ed834cb8f8376334d
florenciamorisse <florenciamorisse@gmail.com>
2022-09-09 12:22:40 -0300

    terminé la página de inicio

---
+++ ahorcado/script.js
@@ -0,0 +1,6 @@
+// var btnIniciar = document.querySelector(".btn-iniciar");
+// btnIniciar.addEventListener("click", ocultarBtn);
+
+// function ocultarBtn() {
+//     btnIniciar.style.display = "none";
+// }
